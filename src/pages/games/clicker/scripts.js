436e20f59ff017586678bfb88d0ef8531c6cab59
Jondolf <joona.m.aalto@outlook.com>
2019-08-30 17:02:46 +0300

    Update Cookie Clicker - Add reset menu - Change costs of upgrades - Game now saves progress - Clean up code - Fix several bugs such as refreshing the interface not working properly - Visual changes such as changing some colors - More responsive

--- src/pages/games/clicker/scripts.js
+++ src/pages/games/clicker/scripts.js
@@ -1,109 +1,116 @@
+// @ts-check
 let pointsElement = document.getElementById("amountOfPoints");
 let priceElement = document.getElementsByClassName("priceElement");
 let upgradeLevel = document.getElementsByClassName("upgradeLevel");
-/**
- * @type {HTMLAudioElement}
- */
-let soundEffectsOn = false;
-let musicOn = false;
-document.getElementById("cookie").addEventListener("click", addPointsByClick);
-//Adds points when the cookie is clicked
+function resetInterval() {
+  clearInterval(pointService.pointInterval);
+  pointService.pointInterval = setInterval(() => pointService.addPointsByTime(pointService.pointsToAddByTime), pointService.time);
+}
+const upgradeService = {
+  defaultUpgradeCosts: [50, 250, 500],
+  timesUpgraded: [0, 0, 0],
 
-let points = 0;
-let pointsToAddByClick = 1;
-let pointsToAddByTime = 1;
-let pointInterval;
-let time = 1000;
+  upgradeLocalStorageSetter() {
+    localStorage.setItem("cookie-clicker-points", pointService.points.toString());
+    localStorage.setItem("cookie-clicker-upgrades", JSON.stringify(this.timesUpgraded));
+  },
 
-document.getElementById("cookie")
-  .addEventListener('mousedown', function (e) { e.preventDefault(); }, false);
-/**
-*
-* @param {MouseEvent} event
-*/
+  getCost(upgradeIndex) {
+    const newUpgradeIndex = this.timesUpgraded[upgradeIndex] + 1; //Adds 1 to the times-upgraded counter
+    const defaultCost = this.defaultUpgradeCosts[upgradeIndex]; //The default cost of the element given by the argument
+    return defaultCost * Math.pow(1.3, newUpgradeIndex - 1); //Changes the default cost
+  },
+  buyUpgrade(upgradeIndex) {
+    if (pointService.points < this.getCost(upgradeIndex)) {
+      return;
+    }
+    pointService.points -= this.getCost(upgradeIndex);
+    this.timesUpgraded[upgradeIndex]++;
+    if (upgradeIndex === 0) {
+      pointService.pointsToAddByClick++;
+      priceElement[0].innerText = Math.floor(this.getCost(upgradeIndex));
+      upgradeLevel[0].innerText = this.timesUpgraded[0];
+    } else if (upgradeIndex === 1) {
+      pointService.time -= 100;
+      resetInterval();
+      priceElement[1].innerText = Math.floor(this.getCost(upgradeIndex));
+      upgradeLevel[1].innerText = this.timesUpgraded[1];
+      refreshPoints();
+    } else if (upgradeIndex === 2) {
+      clearInterval(pointService.pointInterval);
+      pointService.pointsToAddByTime++;
+      pointService.pointInterval = setInterval(() => pointService.addPointsByTime(pointService.pointsToAddByTime), pointService.time);
+      priceElement[2].innerText = Math.floor(this.getCost(upgradeIndex));
+      upgradeLevel[2].innerText = this.timesUpgraded[2];
+      refreshPoints();
+    }
+    refreshPoints();
+    this.upgradeLocalStorageSetter();
+  },
+};
+const musicService = {
+  soundEffectsOn: false,
+  musicOn: false,
 
-function toggleMusic() {
-  musicOn = !musicOn;
-  if (musicOn === true) {
-    document.getElementById("musicOnOff").src = "/images/speaker-icon.svg";
-    document.getElementById("backgroundMusic").play();
-  } else {
-    document.getElementById("musicOnOff").src = "/images/muted-speaker-icon.svg";
-    document.getElementById("backgroundMusic").pause();
-    document.getElementById("backgroundMusic").currentTime = 0;
+  toggleMusic() {
+    this.musicOn = !this.musicOn;
+    if (this.musicOn === true) {
+      document.getElementById("musicOnOff").src = "/images/speaker-icon.svg";
+      document.getElementById("backgroundMusic").play();
+    } else {
+      document.getElementById("musicOnOff").src = "/images/muted-speaker-icon.svg";
+      document.getElementById("backgroundMusic").pause();
+      document.getElementById("backgroundMusic").currentTime = 0;
+    }
+  },
+
+  toggleSoundEffects() {
+    this.soundEffectsOn = !this.soundEffectsOn;
+    if (this.soundEffectsOn === true) {
+      document.getElementById("soundEffectOnOff").innerText = 'SFX ON';
+    } else {
+      document.getElementById("soundEffectOnOff").innerText = 'SFX OFF';
+    }
   }
-}
+};
 
-function toggleSoundEffects() {
-  soundEffectsOn = !soundEffectsOn;
-  if (soundEffectsOn === true) {
-    document.getElementById("soundEffectOnOff").innerText = 'SFX ON';
-  } else {
-    document.getElementById("soundEffectOnOff").innerText = 'SFX OFF';
-  }
-}
-document.getElementById("toggleMusic").addEventListener("click", toggleMusic);
-document.getElementById("toggleSoundEffects").addEventListener("click", toggleSoundEffects);
+const pointService = {
+  points: 0,
+  pointsToAddByClick: 1,
+  pointsToAddByTime: 1,
+  pointInterval: null,
+  time: 2000,
 
-function addPointsByTime(pointAmount) {
-  points += pointAmount;
-  refreshPoints();
-}
+  addPointsByTime(pointAmount) {
+    this.points += pointAmount;
+    localStorage.setItem("cookie-clicker-points", this.points.toString());
+    refreshPoints();
+  },
 
-function addPointsByClick(event) {
-  points += pointsToAddByClick;
-  refreshPoints();
-  if (soundEffectsOn === true) {
-    document.getElementById("soundEffect").currentTime = 0;
-    document.getElementById("soundEffect").play();
-  }
-}
+  addPointsByClick() {
+    this.points += this.pointsToAddByClick;
+    localStorage.setItem("cookie-clicker-points", this.points.toString());
+    refreshPoints();
+    if (musicService.soundEffectsOn === true) {
+      document.getElementById("soundEffect").currentTime = 0;
+      document.getElementById("soundEffect").play();
+    }
+  },
+
+};
+
+document.getElementById("cookie").addEventListener("click", () => pointService.addPointsByClick());
+document.getElementById("cookie").addEventListener('mousedown', function (e) { e.preventDefault(); }, false);
 
 function refreshPoints() {
-  pointsElement.innerText = Math.floor(points.toString());
+  document.getElementById("amountOfPoints").innerText = Math.floor(pointService.points).toString();
 }
 
-const defaultUpgradeCosts = [50, 200, 500, 5000, 100000];
-const upgrades = [0, 0, 0, 0, 0]; //How many times an upgrade has been bought
-
-function upgradeNewCost(upgradeIndex) {
-  const newUpgradeIndex = upgrades[upgradeIndex] + 1; //Adds 1 to the times-upgraded counter
-  const defaultCost = defaultUpgradeCosts[upgradeIndex]; //The default cost of the element given by the argument
-  return defaultCost * Math.pow(1.3, newUpgradeIndex - 1); //Changes the default cost
+function refreshUpgrades() {
+  upgradeService.timesUpgraded = [0, 0, 0]
+  upgradeService.getCost();
 }
 
-function buyUpgrade(upgradeIndex) {
-  if (points < upgradeNewCost(upgradeIndex)) {
-    return;
-  }
-  points -= upgradeNewCost(upgradeIndex);
-  upgrades[upgradeIndex]++;
-  if (upgradeIndex === 0) {
-    pointsToAddByClick++;
-    priceElement[0].innerText = Math.floor(upgradeNewCost(upgradeIndex));
-    upgradeLevel[0].innerText = upgrades[0];
-  } else if (upgradeIndex === 1) {
-    clearInterval(pointInterval);
-    time -= 100;
-    pointInterval = setInterval(() => addPointsByTime(pointsToAddByTime), time);
-    priceElement[1].innerText = Math.floor(upgradeNewCost(upgradeIndex));
-    upgradeLevel[1].innerText = upgrades[1];
-  } else if (upgradeIndex === 2) {
-    clearInterval(pointInterval);
-    pointsToAddByTime++;
-    pointInterval = setInterval(() => addPointsByTime(pointsToAddByTime), time);
-    priceElement[2].innerText = Math.floor(upgradeNewCost(upgradeIndex));
-    upgradeLevel[2].innerText = upgrades[2];
-  } else if (upgradeIndex === 3) {
-    priceElement[3].innerText = Math.floor(upgradeNewCost(upgradeIndex));
-    upgradeLevel[3].innerText = upgrades[3];
-  } else if (upgradeIndex === 4) {
-    priceElement[4].innerText = Math.floor(upgradeNewCost(upgradeIndex));
-    upgradeLevel[4].innerText = upgrades[4];
-  }
-  refreshPoints();
-  //If you have enough points, the cost of the upgrade will be subtracted from your points. Upgrades index +1
-}
 
 const upgradeContainer = document.getElementById("upgradeContainer").style;
 const openUpgradeMenuButton = document.getElementById("openUpgradesMenu");
@@ -111,7 +118,7 @@ let toggleUpgradeMenu = false;
 
 function openUpgradesMenu() {
   toggleUpgradeMenu = !toggleUpgradeMenu;
-  upgradeContainer.zIndex = 2;
+  upgradeContainer.zIndex = "2";
   upgradeContainer.position = "absolute";
   if (toggleUpgradeMenu === true) {
     upgradeContainer.display = "flex";
@@ -119,25 +126,87 @@ function openUpgradesMenu() {
     upgradeContainer.display = "none";
   }
 }
+
 openUpgradeMenuButton.addEventListener("click", openUpgradesMenu);
 
+pointService.points = +localStorage.getItem("cookie-clicker-points");
 
-document.getElementById("upgrade1").addEventListener("click", () => buyUpgrade(0));
-document.getElementById("upgrade2").addEventListener("click", () => buyUpgrade(1));
-document.getElementById("upgrade3").addEventListener("click", () => buyUpgrade(2));
-document.getElementById("upgrade4").addEventListener("click", () => buyUpgrade(3));
-document.getElementById("upgrade5").addEventListener("click", () => buyUpgrade(4));
-priceElement[0].innerText = defaultUpgradeCosts[0];
-priceElement[1].innerText = defaultUpgradeCosts[1];
-priceElement[2].innerText = defaultUpgradeCosts[2];
-priceElement[3].innerText = defaultUpgradeCosts[3];
-priceElement[4].innerText = defaultUpgradeCosts[4];
+document.getElementById("openResetMenu").addEventListener("click", toggleResetMenu);
+document.getElementById("cancelReset").addEventListener("click", closeResetMenu);
+
+let resetProgressMenu = false;
+function toggleResetMenu() {
+  resetProgressMenu = !resetProgressMenu;
+  if (resetProgressMenu === false) {
+    document.getElementById("resetProgress").style.display = "none";
+  } else {
+    document.getElementById("resetProgress").style.display = "block";
+  }
+}
+
+function closeResetMenu() {
+  document.getElementById("resetProgress").style.display = "none";
+}
+
+function resetGameData() {
+  document.getElementById("resetProgress").style.display = "none";
+  localStorage.removeItem("cookie-clicker-points");
+  localStorage.removeItem("cookie-clicker-upgrades");
+  localStorage.removeItem("cookie-clicker-points-to-add-by-click");
+  pointService.points = 0;
+  upgradeService.timesUpgraded = [0, 0, 0];
+  pointService.pointsToAddByClick = 1;
+  pointService.pointsToAddByTime = 1;
+  clearInterval(pointService.pointInterval);
+  refreshPoints();
+  refreshDefaultUpgrades();
+}
+
+upgradeService.timesUpgraded = JSON.parse(localStorage.getItem("cookie-clicker-upgrades")) || upgradeService.timesUpgraded;
+
+document.getElementById("resetGame").addEventListener("click", resetGameData);
+document.getElementById("upgrade1").addEventListener("click", () => upgradeService.buyUpgrade(0));
+document.getElementById("upgrade2").addEventListener("click", () => upgradeService.buyUpgrade(1));
+document.getElementById("upgrade3").addEventListener("click", () => upgradeService.buyUpgrade(2));
+
+function refreshDefaultUpgrades() {
+  priceElement[0].innerText = upgradeService.defaultUpgradeCosts[0];
+  priceElement[1].innerText = upgradeService.defaultUpgradeCosts[1];
+  priceElement[2].innerText = upgradeService.defaultUpgradeCosts[2];
+
+  upgradeLevel[0].innerText = upgradeService.timesUpgraded[0];
+  upgradeLevel[1].innerText = upgradeService.timesUpgraded[1];
+  upgradeLevel[2].innerText = upgradeService.timesUpgraded[2];
+}
+
+function refreshCurrentUpgrades() {
+  document.getElementById("upgrade1").innerText;
+}
 
 
-upgradeLevel[0].innerText = upgrades[0];
-upgradeLevel[1].innerText = upgrades[1];
-upgradeLevel[2].innerText = upgrades[2];
-upgradeLevel[3].innerText = upgrades[3];
-upgradeLevel[4].innerText = upgrades[4];
+document.getElementById("toggleMusic").addEventListener("click", () => musicService.toggleMusic());
+document.getElementById("toggleSoundEffects").addEventListener("click", () => musicService.toggleSoundEffects());
 document.getElementById("musicOnOff").src = "/images/muted-speaker-icon.svg";
 document.getElementById("soundEffectOnOff").innerText = "SFX OFF";
+
+pointService.pointsToAddByClick = upgradeService.timesUpgraded[0] + 1;
+
+pointService.time = pointService.time - upgradeService.timesUpgraded[1] * 100;
+if (upgradeService.timesUpgraded[1] > 0) {
+  resetInterval();
+}
+
+pointService.pointsToAddByTime = upgradeService.timesUpgraded[2] + 1;
+
+pointService.pointsToAddByClick;
+pointService.time;
+pointService.pointsToAddByTime;
+refreshPoints();
+
+priceElement[0].innerText = Math.round(upgradeService.getCost(0)).toString();
+priceElement[1].innerText = Math.round(upgradeService.getCost(1)).toString();
+priceElement[2].innerText = Math.round(upgradeService.getCost(2)).toString();
+
+upgradeLevel[0].innerText = upgradeService.timesUpgraded[0].toString();
+upgradeLevel[1].innerText = upgradeService.timesUpgraded[1].toString();
+upgradeLevel[2].innerText = upgradeService.timesUpgraded[2].toString();
