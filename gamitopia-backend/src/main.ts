eb13265ab147b1cc7d5eee8cbbbf540f78bcbfa0
Jondolf <joona.m.aalto@outlook.com>
2020-02-23 17:32:43 +0200

    Get backend port from env variable

--- gamitopia-backend/src/main.ts
+++ gamitopia-backend/src/main.ts
@@ -5,6 +5,6 @@ import * as helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
