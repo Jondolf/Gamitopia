a8eb69e741ad8c3143b089433953fdd3994d9fb7
Jondolf <joona.m.aalto@outlook.com>
2020-05-09 19:19:15 +0300

    Make page title change based on the route For example the route /home will have a title of Home

--- src/router.ts
+++ src/router.ts
@@ -6,40 +6,59 @@ let isAdmin = localStorage.getItem('jwt');
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   routes: [
     // Main pages
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
+      meta: {
+        title: 'Home | Gamitopia'
+      }
     },
     {
       path: '/games',
       name: 'games',
-      component: () => import('./views/Games.vue')
+      component: () => import('./views/Games.vue'),
+      meta: {
+        title: 'Games | Gamitopia'
+      }
     },
     {
       path: '/news',
       name: 'news',
-      component: () => import('./views/News.vue')
+      component: () => import('./views/News.vue'),
+      meta: {
+        title: 'News | Gamitopia'
+      }
     },
     {
       path: '/news/:id',
-      component: () => import('./views/news/IndividualNewsPostView.vue')
+      component: () => import('./views/news/IndividualNewsPostView.vue'),
+      beforeEnter: (to, from, next) => {
+        document.title = `News post #${to.params.id} | Gamitopia`;
+        next();
+      }
     },
     {
       path: '/about',
       name: 'about',
-      component: () => import('./views/About.vue')
+      component: () => import('./views/About.vue'),
+      meta: {
+        title: 'About | Gamitopia'
+      }
     },
 
     // Admin
     {
       path: '/admin/login',
       name: 'admin-login',
-      component: () => import('./views/admin/AdminLogin.vue')
+      component: () => import('./views/admin/AdminLogin.vue'),
+      meta: {
+        title: 'Admin login | Gamitopia'
+      }
     },
     {
       path: '/admin/create-news-post',
@@ -51,6 +70,9 @@ export default new Router({
         } else {
           next('admin/login');
         }
+      },
+      meta: {
+        title: 'Create news post | Gamitopia'
       }
     },
     {
@@ -59,6 +81,7 @@ export default new Router({
       component: () => import('./views/admin/NewsPostEdit.vue'),
       beforeEnter: (to, from, next) => {
         if (isAdmin) {
+          document.title = `Edit news post #${to.params.id} | Gamitopia`;
           next();
         } else {
           next('admin/login');
@@ -70,38 +93,66 @@ export default new Router({
     {
       path: '/cookie-clicker',
       name: 'cookie-clicker',
-      component: () => import('./views/games/CookieClicker.vue')
+      component: () => import('./views/games/CookieClicker.vue'),
+      meta: {
+        title: 'Cookie Clicker | Gamitopia'
+      }
     },
     {
       path: '/randomizer',
       name: 'randomizer',
-      component: () => import('./views/games/Randomizer.vue')
+      component: () => import('./views/games/Randomizer.vue'),
+      meta: {
+        title: 'Randomizer | Gamitopia'
+      }
     },
     {
       path: '/tic-tac-toe',
       name: 'tic-tac-toe',
-      component: () => import('./views/games/Tic-Tac-Toe.vue')
+      component: () => import('./views/games/Tic-Tac-Toe.vue'),
+      meta: {
+        title: 'Tic-tac-toe | Gamitopia'
+      }
     },
     {
       path: '/adventura',
       name: 'adventura',
-      component: () => import('./views/games/Adventura.vue')
+      component: () => import('./views/games/Adventura.vue'),
+      meta: {
+        title: 'Adventura | Gamitopia'
+      }
     },
     {
       path: '/snake',
       name: '/snake',
-      component: () => import('./views/games/Snake.vue')
+      component: () => import('./views/games/Snake.vue'),
+      meta: {
+        title: 'Snake | Gamitopia'
+      }
     },
     {
       path: '/card',
       name: '/card',
-      component: () => import('./views/games/CardGame.vue')
+      component: () => import('./views/games/CardGame.vue'),
+      meta: {
+        title: 'Card game | Gamitopia'
+      }
     },
     {
       path: '/target-practise',
       name: 'targetpractise',
       component: () =>
-        import('./views/games/target-practise/TargetPractise.vue')
+        import('./views/games/target-practise/TargetPractise.vue'),
+      meta: {
+        title: 'Target Practise | Gamitopia'
+      }
     }
   ]
 });
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title;
+  next();
+});
+
+export default router;
