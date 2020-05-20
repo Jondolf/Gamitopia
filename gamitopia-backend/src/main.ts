6de83642746445915c5f8cd74eda1bf3af804bf3
Jondolf <joona.m.aalto@outlook.com>
2020-05-20 18:08:38 +0300

    Redirect HTTP to HTTPS with express-sslify

--- gamitopia-backend/src/main.ts
+++ gamitopia-backend/src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as helmet from 'helmet';
+import * as enforce from 'express-sslify';
 import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
+  if (
+    process.env
+      .NODE_ENV /* can be any Heroku's env variable, just checking that it's not localhost */
+  ) {
+    app.use(enforce.HTTPS({ trustProtoHeader: true }));
+  } // redirects HTTP to HTTPS
   app.use(compression());
   await app.listen(process.env.PORT || 3000);
 }
