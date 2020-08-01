61995e05262ea1d83b0422b38a57f19992abaf89
Jondolf <joona.m.aalto@outlook.com>
2020-08-01 18:34:02 +0300

    Add GamePage component Used as the root component of all game views, as they are very similar. It adds the header and description of the given game automatically. The game's component will be put inside this component (in the slot). This greatly reduces duplicated code and makes it easy to style all game pages at once. This will be added to games soon.

---
+++ src/components/games/GamePage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="game-page">
+    <GameHeader :game="game" />
+    <slot><!-- Game container comes here --></slot>
+    <GameDescription :game="game" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import GameHeader from '@/components/games/GameHeader.vue';
+import GameDescription from '@/components/games/GameDescription.vue';
+import { GameInfo } from '../../interfaces/GameInfo';
+
+export default Vue.extend({
+  name: 'GamePage',
+
+  components: {
+    GameHeader,
+    GameDescription
+  },
+
+  props: {
+    game: Object as () => GameInfo
+  }
+});
+</script>
+
+<style scoped>
+.game-page {
+  padding-top: 66px;
+}
+</style>
