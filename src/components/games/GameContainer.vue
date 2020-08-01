9ca1385590c027bcab41867d542297b1c1f94e7b
Jondolf <joona.m.aalto@outlook.com>
2020-08-01 18:28:18 +0300

    Add GameContainer component Used as the container of games, has fullscreen functionality, custom default size, default styles etc. This helps greatly at minimizing duplicated code. This is not yet used in games, but soon will be.

---
+++ src/components/games/GameContainer.vue
@@ -0,0 +1,53 @@
+<template>
+  <div
+    :style="
+      isFullscreen ? { width: '100%', height: '100%' } : { width, height }
+    "
+    :class="isFullscreen ? 'game-container fullscreen' : 'game-container'"
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'GameContainer',
+
+  props: {
+    width: String,
+    height: String,
+    isFullscreen: Boolean
+  },
+
+  watch: {
+    isFullscreen() {
+      document.body.style.overflow = this.isFullscreen ? 'hidden' : 'auto';
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.game-container {
+  position: relative;
+  margin: auto;
+  border: 2px solid var(--color-light-contrast);
+  border-radius: 5px;
+  font-family: 'Nunito', 'Nunito Sans', sans-serif;
+  box-sizing: border-box;
+  overflow: hidden;
+
+  &.fullscreen {
+    border: none;
+    border-radius: 0;
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 20;
+  }
+}
+</style>
