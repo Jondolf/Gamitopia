8cf6e5a168718789ff8e53ea2cf443fd71c50a04
Jondolf <joona.m.aalto@outlook.com>
2020-08-11 19:33:34 +0300

    Change games' routes' names

--- src/router/games.ts
+++ src/router/games.ts
@@ -2,64 +2,64 @@ import { RouteRecordRaw } from 'vue-router';
 
 const gameRoutes: Array<RouteRecordRaw> = [
   {
-    path: '/cookie-clicker/',
-    name: 'cookie-clicker',
+    path: '/cookie-clicker',
+    name: 'CookieClicker',
     component: () => import('@/views/games/CookieClicker.vue'),
     meta: {
       title: 'Cookie Clicker | Gamitopia'
     }
   },
   {
-    path: '/randomizer/',
-    name: 'randomizer',
+    path: '/randomizer',
+    name: 'Randomizer',
     component: () => import('@/views/games/Randomizer.vue'),
     meta: {
       title: 'Randomizer | Gamitopia'
     }
   },
   {
-    path: '/tic-tac-toe/',
-    name: 'tic-tac-toe',
+    path: '/tic-tac-toe',
+    name: 'TicTacToe',
     component: () => import('@/views/games/TicTacToe.vue'),
     meta: {
-      title: 'Tic-tac-toe | Gamitopia'
+      title: 'Tic-Tac-Toe | Gamitopia'
     }
   },
   {
-    path: '/adventura/',
-    name: 'adventura',
+    path: '/adventura',
+    name: 'Adventura',
     component: () => import('@/views/games/Adventura.vue'),
     meta: {
       title: 'Adventura | Gamitopia'
     }
   },
   {
-    path: '/snake/',
-    name: '/snake',
+    path: '/snake',
+    name: 'Snake',
     component: () => import('@/views/games/Snake.vue'),
     meta: {
       title: 'Snake | Gamitopia'
     }
   },
   {
-    path: '/calculator/',
-    name: 'calculator',
+    path: '/calculator',
+    name: 'Calculator',
     component: () => import('@/views/games/Calculator.vue'),
     meta: {
       title: 'Calculator | Gamitopia'
     }
   },
   {
-    path: '/card/',
-    name: 'card',
+    path: '/card',
+    name: 'Card',
     component: () => import('@/views/games/CardGame.vue'),
     meta: {
       title: 'Card game | Gamitopia'
     }
   },
   {
-    path: '/target-practise/',
-    name: 'targetpractise',
+    path: '/target-practise',
+    name: 'TargetPractise',
     component: () =>
       import('@/views/games/target-practise/TargetPractise.vue'),
     meta: {
