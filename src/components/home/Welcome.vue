b670a93c5678c19b26fea4759ef6e72cd07a2186
Jondolf <joona.m.aalto@outlook.com>
2020-08-30 14:40:44 +0300

    Change Gamitopia logo & favicons, update Welcome - Updated the logo again, made it a bit better - Added the text "1st Anniversary" to Welcome component

--- src/components/home/Welcome.vue
+++ src/components/home/Welcome.vue
@@ -2,6 +2,7 @@
   <div class="welcome">
     <span class="welcome-to-text">Welcome To</span>
     <img src="@/assets/images/gamitopia_logo_wide.png" alt="Gamitopia wide logo" class="gamitopia-text" />
+    <p class="anniversary-text">1st Anniversary</p>
   </div>
 </template>
 
@@ -23,6 +24,7 @@ export default defineComponent({
   justify-content: center;
   align-items: center;
   box-sizing: border-box;
+  font-size: 20px;
 
   .welcome-to-text {
     border-bottom: 2px solid white;
@@ -32,6 +34,14 @@ export default defineComponent({
     width: 60vh;
     max-width: 80vw;
     margin: 5px 0 5px 0;
+  }
+
+  .anniversary-text {
+    background: linear-gradient(90deg, rgb(255, 255, 0), rgb(255, 100, 50));
+    color: transparent;
+    font-weight: bold;
+    background-clip: text;
+    -webkit-background-clip: text;
   }
 }
 </style>
