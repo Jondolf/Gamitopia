3bf34954daf606491cb387935a92ccb33038be76
Jondolf <joona.m.aalto@outlook.com>
2020-12-11 21:22:19 +0200

    Remove comment

--- src/store/games.ts
+++ src/store/games.ts
@@ -36,11 +36,6 @@ const allGames: AllGames = {
     description: {
       shortDescription: `Sneak past guards without going into light.`,
       longDescription: inTheShadowsDescription,
-      /*        `**Note:** this game is a demo and may be expanded and improved later.
-      
-      Sneak past guards as you wander through each room.
-      
-      You are a thief whose task is to steal the incredibly valuable diamond. To stay hidden you must stay out of the guards' lights and utilize the different obstacles.`,*/
       madeWith: ['Godot (with GDScript)'],
       support: []
     }
